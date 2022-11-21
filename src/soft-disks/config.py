6b05156c38c04a7b95e60dbab9a089382a036bf7
Meridian-Onset <jma64wright@gmail.com>
2022-11-21 12:11:18 +0100

    woop woop changes

--- src/soft-disks/config.py
+++ src/soft-disks/config.py
@@ -3,19 +3,19 @@ import numpy as np
 scaler = 1e20
 
 k_b_real = 1.380649e-23      # Real units (m^2 kg s^-2 K^-1)
-k_b = k_b_real * scaler
+k_b = 1
 T = 274                 # Kelvin
 m = 1                   # Mass of particles
 L = 10                  # box size in nm?
 N = 100                 # number of particles
 eta = 0.72              # volume fraction
 rho = N/L**2            # number density
-sigma = 2*np.sqrt(eta/N/np.pi)   # particle diameter
+sigma = 1   # particle diameter
 dr = sigma/20           # binning interval for the correlation function
 Nsteps = 1000   #Number of steps involved in the simulation
-
-dt = 1e-9
+epsilon = 1 # Factor for LJ Potential
+dt = 1e-3
 
 
 init_spacing = L/(np.sqrt(N)+0.2)
-velocity_variance = k_b_real*T 
+velocity_variance = T 
--- src/soft-disks/evolve.py
+++ src/soft-disks/evolve.py
@@ -1,15 +1,58 @@
 import numpy as np
 from numpy.typing import ArrayLike
 
-from config import N
+from typing import Tuple
+
+from config import N, dt, L
 from utils import plot
 from build_ensemble import hex_build, square_build
 
-def move(starting_positions: ArrayLike):
+def move(starting_configuration: Tuple[ArrayLike], timestep: float = dt):
+    """
+    Updates the positions in 
+    `starting_configuration` with the particles' 
+    associated velocities.
     
+    Parameters:
+        - `starting_configuration` (Tuple
+        [ArrayLike], required): The Tuple of 
+        position and velocity arrays that define 
+        the current configuration.
+        - `timestep` (float, optional): The length 
+        of time to evolve over, uses the `dt` 
+        parameter in the config.py file by default
+    Returns:
+        The updated position array, the forces and 
+        thus the velocities are calculated from 
+        the new position later.
+    """
+    positions = np.empty((N,2))
+    _positions = starting_configuration[0] + timestep * starting_configuration[1]
+    for i, position in enumerate(_positions):
+        _x, _y = position
+        while True:
+            if _x < 0 or _x > L:
+                _x = _x - np.sign(_x)*L
+            if _y < 0 or _y > L:
+                _y = _y - np.sign(_y)*L
+            else:
+                break
+        positions[i] = [_x, _y]
+    return positions
 
+def find_distances(position_array: ArrayLike) -> ArrayLike:
+    pass
+
+def force(position_array: ArrayLike) -> ArrayLike:
+    pass
+    
 def equilibriate():
     pass
 
 
 
+if __name__=="__main__":
+    from build_ensemble import square_build
+    from utils import plot
+
+    plot(move(square_build()))
